72ef6dd04ee94f301bf391e7d10279a3b3ec7180
Andy <andyloox@gmail.com>
2022-03-23 16:29:35 +0300

    top250

--- js/top250.js
+++ js/top250.js
@@ -1,7 +1,7 @@
 	$.ajax({
 			 		async: true,
 			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1',
 					type: 'GET',
 			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
 					success: function(response){
@@ -13,45 +13,12 @@
 							}
 					}
 	});
-	 
-	$.ajax({
-			 		async: true,
-			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=2',
-					type: 'GET',
-			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
-					success: function(response){
-	         				var a = response.films;
-			 				var index, len;
-			 				for (index = 0, len = a.length; index < len; ++index) {
-				 				var item = a[index];
-					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.filmId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span>    <span class="year">'+item.year+'</span></span></div></a></li>');
-							}
-					}
-	});
-	 
-	$.ajax({
-			 		async: true,
-			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=3',
-					type: 'GET',
-			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
-					success: function(response){
-	         				var a = response.films;
-			 				var index, len;
-			 				for (index = 0, len = a.length; index < len; ++index) {
-				 				var item = a[index];
-					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.filmId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span>    <span class="year">'+item.year+'</span></span></div></a></li>');
-							}
-					}
-	});
-	
 function search(){
 	var text = document.getElementById('text').value;
 	$.ajax({
 			 		async: true,
 			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='+text,
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.1/films/top?type=TOP_250_BEST_FILMS&page='+text,
 					type: 'GET',
 			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
 					success: function(response){
