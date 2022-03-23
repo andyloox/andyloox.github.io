de232ffedf5d7978e79705953c3cf95cf858d744
Andy <andyloox@gmail.com>
2022-03-23 16:36:50 +0300

    js new!

--- js/popular.js
+++ js/popular.js
@@ -13,7 +13,9 @@
 							}
 					}
 	});
-	 
+	
+
+ 
 	$.ajax({
 			 		async: true,
 			 		crossDomain: true,
@@ -29,7 +31,9 @@
 							}
 					}
 	});
-	 
+	
+
+ 
 	$.ajax({
 			 		async: true,
 			 		crossDomain: true,
@@ -45,7 +49,39 @@
 							}
 					}
 	});
-	
+
+	$.ajax({
+			 		async: true,
+			 		crossDomain: true,
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=4',
+					type: 'GET',
+			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
+					success: function(response){
+	         				var a = response.films;
+			 				var index, len;
+			 				for (index = 0, len = a.length; index < len; ++index) {
+				 				var item = a[index];
+					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.filmId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span>    <span class="year">'+item.year+'</span></span></div></a></li>');
+							}
+					}
+	});
+
+	$.ajax({
+			 		async: true,
+			 		crossDomain: true,
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=5',
+					type: 'GET',
+			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
+					success: function(response){
+	         				var a = response.films;
+			 				var index, len;
+			 				for (index = 0, len = a.length; index < len; ++index) {
+				 				var item = a[index];
+					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.filmId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span>    <span class="year">'+item.year+'</span></span></div></a></li>');
+							}
+					}
+	});
+
 function search(){
 	var text = document.getElementById('text').value;
 	$.ajax({
@@ -53,7 +89,7 @@ function search(){
 			 		crossDomain: true,
 					url: 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='+text,
 					type: 'GET',
-			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
+			 		headers: {'X-API-KEY':'1281ba0d-593b-4f3e-8b14-2b63e66ae00a'},
 					success: function(response){
 							$('.title').empty();
 							$('.items').empty();
--- js/premiere.js
+++ js/premiere.js
@@ -1,26 +1,30 @@
 	$.ajax({
 			 		async: true,
 			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2022&month=FEBRUARY',
 					type: 'GET',
 			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
 					success: function(response){
-	         				var a = response.films;
+	         				var a = response.items;
 			 				var index, len;
 			 				for (index = 0, len = a.length; index < len; ++index) {
 				 				var item = a[index];
-					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.filmId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span>    <span class="year">'+item.year+'</span></span></div></a></li>');
+					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.kinopoiskId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span> <span class="year">'+item.premiereRu+'</span></span></div></a></li>');
 							}
 					}
 	});
-	 
+
+function search(){
+	var text = document.getElementById('text').value;
 	$.ajax({
 			 		async: true,
 			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=2',
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='+text,
 					type: 'GET',
 			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
 					success: function(response){
+							$('.title').empty();
+							$('.items').empty();
 	         				var a = response.films;
 			 				var index, len;
 			 				for (index = 0, len = a.length; index < len; ++index) {
@@ -29,23 +33,32 @@
 							}
 					}
 	});
-	 
-	$.ajax({
-			 		async: true,
-			 		crossDomain: true,
-					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=3',
-					type: 'GET',
-			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
-					success: function(response){
-	         				var a = response.films;
-			 				var index, len;
-			 				for (index = 0, len = a.length; index < len; ++index) {
-				 				var item = a[index];
-					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.filmId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span>    <span class="year">'+item.year+'</span></span></div></a></li>');
-							}
-					}
-	});
+	};
 	
+$(document).ready(function(){
+                 $("#text").keypress(function(e){
+                   if(e.keyCode==13){
+                   //нажата клавиша enter - здесь ваш код
+                   search();
+                   }
+                 });
+ 
+             });	$.ajax({
+			 		async: true,
+			 		crossDomain: true,
+					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2022&month=FEBRUARY',
+					type: 'GET',
+			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
+					success: function(response){
+	         				var a = response.items;
+			 				var index, len;
+			 				for (index = 0, len = a.length; index < len; ++index) {
+				 				var item = a[index];
+					 				 $('.items').append('<li class="item" style="margin-bottom: 1rem">    <a class="link" href="/films?id='+item.kinopoiskId+'&title='+item.nameRu+' ('+item.year+')"  title="'+item.nameRu+' ('+item.year+')">        <div class="cover-wrap">          <div class="cover js-b-lazy fadeIn animated" style="background-image: url('+item.posterUrlPreview+');"></div></div>     <div class="description">     <h3 class="title">'+item.nameRu+'</h3>      <span class="desc">     <span class="rating">'+item.rating+'</span> <span class="year">'+item.premiereRu+'</span></span></div></a></li>');
+							}
+					}
+	});
+
 function search(){
 	var text = document.getElementById('text').value;
 	$.ajax({
