bbbe0cfc7276d018a83c02fac3e6898853e0e431
Andy <59578532+andyloox@users.noreply.github.com>
2022-11-13 11:26:19 +0300

    Update top250.js

--- js/top250.js
+++ js/top250.js
@@ -3,7 +3,7 @@
 			 		crossDomain: true,
 					url: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1',
 					type: 'GET',
-			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
+			 		headers: {'X-API-KEY':'525477d9-623c-428d-a17e-59a4fc457261'},
 					success: function(response){
 	         				var a = response.films;
 			 				var index, len;
@@ -20,7 +20,7 @@ function search(){
 			 		crossDomain: true,
 					url: 'https://kinopoiskapiunofficial.tech/api/v2.1/films/top?type=TOP_250_BEST_FILMS&page='+text,
 					type: 'GET',
-			 		headers: {'X-API-KEY':'e559c52a-6911-41c1-aaea-7781ac45ef2e'},
+			 		headers: {'X-API-KEY':'525477d9-623c-428d-a17e-59a4fc457261'},
 					success: function(response){
 							$('.title').empty();
 							$('.items').empty();
